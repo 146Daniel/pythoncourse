cca973c188edc098c4b591b33ab1b9fc334cdec3
146Daniel <88628006+146Daniel@users.noreply.github.com>
2025-02-24 16:45:30 +0200

    Add files via upload

---
+++ first lesson24.2.py
@@ -0,0 +1,26 @@
+#targil 1
+height = float(input(" Hello, Enter the height of the rectangle: "))
+wight = float(input(" Hello, Enter the weight of the rectangle: "))
+print(height * wight)
+
+#targil2
+#listofnumbers = list[1,2,3]
+#listofnumbers = input("please enter three numbers")
+#result = listofnumbers[0] + listofnumbers[1] + listofnumbers[2] / 3
+#print(result)
+
+#targil3
+age = int(input("what is your age"))
+print(100-age)
+
+#targil4
+tempC = float(input("what is the Temp? "))
+tempF = (tempC*9/5)+32
+print(tempF)
+
+#targil5
+price = float(input("what is the price?"))
+sale = float(input("what is the sale percent"))
+sale = sale / 100
+finalprice = price * sale
+print("the price is:", finalprice)
